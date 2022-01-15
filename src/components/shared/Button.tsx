42b004dc7ed05b0ed930bf999697375c8734fbfc
Xavier <isiomastewart@gmail.com>
2022-01-15 15:05:06 +0100

    initial commit

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type ButtonProps = {
+  rendered: JSX.Element,
+  variant: string,
+  renderedColor?: string,
+  classNom: string,
+  inverted?: boolean
+}
+
+type ButtonState = {
+  buttonStyles: Object
+}
+
+class Button extends React.Component<ButtonProps, ButtonState> {
+  
+  constructor(props: ButtonProps){
+    super(props);
+    this.state = {
+      buttonStyles: {
+        display: "inline-block",
+        fontWeight: 500,
+        textAlign: "center",
+        verticalAlign: "center",
+        borderRadius: "20%",
+        backgroundColor: !this.props.inverted ? this.props.variant : this.props.renderedColor,
+        color: !this.props.inverted ? this.props.renderedColor : this.props.variant,
+        margin: "auto",
+        cursor: "pointer",
+        padding: "8 px 12 px",
+        boxShadow: "1 px 2 px 3 px rgba(0, 0, 0, 0.2)",
+        border: this.props.inverted ? "2px solid {this.props.renderedColor}" : 0
+      }
+    }
+  }
+  render(){
+    return (
+    <button style={this.state.buttonStyles} className={this.props.classNom}>
+      {this.props.rendered}
+    </button> );
+  }
+}
+
+export default Button;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import "./main.scss";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
