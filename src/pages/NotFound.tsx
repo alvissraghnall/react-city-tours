b28631f85db06a89226d1a648d9e8d69306892ac
Alviss Raghnall <isiomastewart@gmail.com>
2022-06-25 14:10:27 +0100

    Skepta - Greaze Mode

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleLeftIcon from '@material-ui/icons/ArrowCircleLeft';
 
 const WrapperDiv = styled.div`
   text-align: center;
